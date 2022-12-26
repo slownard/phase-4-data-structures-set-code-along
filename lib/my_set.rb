3da01904efcc8b89fa762ca3122bd3afbed0f743
Bernard Legaspi <blegaspi26@gmail.com>
2022-12-25 19:41:47 -0500

    commit tree dd5df7038d62f2b35d0317137db3c971c3dd90c6

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,33 @@
 # your code here
+
+#a set is a data structurre that is used for storing a collection of unique value
+
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+          @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        #returns true or false
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true # add a value as a key on the hash
+        self                # return the updated set
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end  
+
+    #return number of elements in the set 
+    def size
+        @hash.size
+    end
+
+end
